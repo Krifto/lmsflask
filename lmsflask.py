07f21d1a8e30af3daec022560dc8a894fd7ac50b
Tim Horz <timhorz@gmail.com>
2013-12-15 01:35:27 +0100

    playing around with flask and jinja template

--- lmsflask.py
+++ lmsflask.py
@@ -1,33 +1,50 @@
+import sys
+reload(sys)
+sys.setdefaultencoding('utf-8')
+
+
 from pylms.server import Server
 from pylms.player import Player
-import os
-
-def lmstest():
-    sc = Server(hostname="nook-color-android", port=9090)
-    sc.connect()
-
-    print "Logged in: %s" % sc.logged_in
-    print "Version: %s" % sc.get_version()
-
-    players = sc.get_players()
-    for player in players:
-        info = "Name: %s | Mode: %s | Time: %s | Connected: %s | WiFi: %s" % (player.get_name(), player.get_mode(), player.get_time_elapsed(), player.is_connected, player.get_wifi_signal_strength())
-    #sq.stop()
-        print(info)
-    #return info
-    #print sq.get_track_title()
-    #print sq.get_time_remaining()
 
 
+#def lmstest():
+sc = Server(hostname="192.168.178.24", port=9090)
+sc.connect()
 
+print "Logged in: %s" % sc.logged_in
+print "Version: %s" % sc.get_version()
 
-from flask import Flask
+from flask import Flask, render_template
 app = Flask(__name__)
+
+commands = ('play', 'pause', 'stop', 'volume_up', 'volume_down')
+
+
+def player_info_string(player):
+    return "Name: %s | Mode: %s | Time: %s | Connected: %s | WiFi: %s" % (player.get_name(), player.get_mode(), player.get_time_elapsed(), player.is_connected, player.get_wifi_signal_strength())
+
 
 @app.route('/')
 def hello_world():
-    lmstest()
-    return 'Hello World!'
+    return render_template('lmsflask.html', players=sc.get_players())
+
+
+@app.route('/<player_name>/<cmd>')
+def player_control(player_name, cmd):
+    player = sc.get_player(player_name)
+    if not player:
+        return "error, player not found"
+    if cmd in commands:
+        if cmd == 'play':
+            player.play()
+        elif cmd == 'stop':
+            player.stop()
+        elif cmd == 'volume_up':
+             player.volume_up()
+        elif cmd == 'volume_down':
+             player.volume_down()
+    return render_template('lmsflask.html', players=sc.get_players())
+
 
 if __name__ == '__main__':
     app.debug = True
